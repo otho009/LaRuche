1e841c9e6718249f7266d2b4d0d9e47101e37ffa
Rima Bachar <bachar.rima@etu.umontpellier.fr>
2017-11-14 18:03:26 +0100

    added tools and technologies to the README file

---
+++ prototype/includes/footer.php
@@ -0,0 +1,5 @@
+<ul>
+  <li><a href="footer_pages/faq.html">FAQ</a></li>
+  <li><a href="footer_pages/terms_conditions.html">Terms &amp; Conditions</a></li>
+  <li><a href="footer_pages/about.html">About</a></li>
+</ul>
---
+++ prototype/includes/header.php
@@ -0,0 +1,2 @@
+<img src="images/logo.gif" alt="logo de LaRuche" height="120px" width="120px" />
+<h1>LaRuche !</h1>
