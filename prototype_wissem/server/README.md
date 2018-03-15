763499d574ffc4544c6ac34a95c7095368d2272e
ONIZUKAGTO <wiwi.sam96@outlook.it>
2018-03-15 14:52:02 +0100

    remove of synfony server

--- prototype_wissem/server/README.md
+++
@@ -1,4 +0,0 @@
-server
-======
-
-A Symfony project created on January 28, 2018, 3:28 pm.
--- prototype_wissem/server/src/BOW/PlatformBundle/BOWPlatformBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace BOW\PlatformBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class BOWPlatformBundle extends Bundle
-{
-}
