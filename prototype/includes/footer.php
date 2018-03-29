ee3c871c3f5edca06ee3f869992c7eef71b6277a
ONIZUKAGTO <wiwi.sam96@outlook.it>
2018-03-29 17:36:09 +0200

    deleted the directory prototype

--- prototype/includes/footer.php
+++
@@ -1,5 +0,0 @@
-<ul>
-  <li><a href="footer_pages/faq.html">FAQ</a></li>
-  <li><a href="footer_pages/terms_conditions.html">Terms &amp; Conditions</a></li>
-  <li><a href="footer_pages/about.html">About</a></li>
-</ul>
--- prototype/includes/header.php
+++
@@ -1,2 +0,0 @@
-<img src="images/logo.gif" alt="logo de LaRuche" height="120px" width="120px" />
-<h1>LaRuche !</h1>
