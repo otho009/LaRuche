6abdc9dffbae4f000e496175558ced9be2ebc255
ONIZUKAGTO <wiwi.sam96@outlook.it>
2018-03-29 17:28:31 +0200

    saved things for prototype_wissem before pulling othmane prototype

--- prototype_wissem/server/src/js/client.js
+++ prototype_wissem/server/src/js/client.js
@@ -1,7 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from 'react-router-dom';
+import Bootstrap from "./vendor/bootstrap.native";
 
-import Layout from "./components/Layout";
+//import web pages
+import Layout from "./pages/Layout";
+import SignIn from "./pages/SignIn";
+import WhoWeAre from "./pages/WhoWeAre";
 
-const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+//target the div that will be rendered
+let app = document.getElementById('app');
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Route path="/" component={Layout} />
+  </BrowserRouter>,
+  app);
---
+++ prototype_wissem/server/src/js/pages/WhoWeAre.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default class WhoWeAre extends React.Component {
+  render() {
+    return (
+      <h1>Who we are.</h1>
+    );
+  }
+}
