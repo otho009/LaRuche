08fdf35ff58cb3a55ce30c114c19e86caedb5532
ONIZUKAGTO <wiwi.sam96@outlook.it>
2018-04-15 17:04:37 +0200

    cleaned the code

--- main-app/src/Containers/Main.js
+++ main-app/src/Containers/Main.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
-import Header from "../Components/Header.js"
-import Footer from "../Components/Footer.js"
 import Accueil from "./Accueil.js";
 import AboutUs from "./AboutUs.js";
 import SignIn from "./SignIn.js";
