209e36283c46f8758a4b4b9f5d33be2ab6d95a79
ONIZUKAGTO <wiwi.sam96@outlook.it>
2018-04-19 12:44:14 +0200

    Ajouté le css de AboutUs de Othmane

--- main-app/src/App.js
+++ main-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Main from "./Containers/Main.js";
-import ".App.css";
+import "./App.css";
 import { Switch, Route } from "react-router-dom";
 
 export default class App extends Component {
