3769532d6149ef655b586a1b9c6a221bd3539ae8
Farajallah Othmane <othmane.farjallah@etu.umontpellier.fr>
2018-05-05 21:51:20 +0200

    made the header as prettier as bachar's face

--- main-app/src/Components/Header2.js
+++ main-app/src/Components/Header2.js
@@ -8,63 +8,74 @@ export default class Header2 extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-md navbar-dark bg-primary">
+        <nav class="navbar navbar-expand-md navbar-dark bg-primary">
+          <a class="navbar-brand">
             <NavLink exact to="/" style={{ color: "white" }} activeStyle={{ color: "white" }} >
               CourtCircuit
             </NavLink>
+          </a>
+          <a class="navbar-brand mx-auto p-auto">
             <NavLink exact to="/">
-              <img src={logo} height="60" className="d-inline-block align-top" alt="" />
+              <img src={logo} height="60" class="d-inline-block align-top" alt="" />
             </NavLink>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
         </button>
-          <div className="collapse navbar-collapse text-right" id="navbarCollapse">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item nav-link nav-link ">
-                  <NavLink exact to="/" style={{ color: "white" }} activeStyle={{ color: "black" }} >
-                    <i className="fas fa-home" /> Accueil
-                  </NavLink>
-              </li>
-              <li className="nav-item nav-link">
-                  <NavLink to="/about" style={{ color: "white" }} activeStyle={{ color: "black" }} >
-                    <i className="fas fa-users" /> A propos
-                  </NavLink>
-              </li>
-              <li className="nav-item nav-link">
-                  <NavLink to="/SignIn" style={{ color: "white" }} activeStyle={{ color: "black" }} >
-                    <i className="fas fa-sign-in-alt" /> Identifiez vous
-                  </NavLink>
-              </li>
-              <li className="nav-item nav-link">
-                  <NavLink to="/about"style={{ color: "white" }} activeStyle={{ color: "white" }} >
-                    <i className="fas fa-address-book fa-fw"> </i> Contact
-                  </NavLink>
-              </li>
-              <li className="nav-item nav-link">
-                <div className="input-group mb-3">
-                  <div className="input-group-prepend">
-                    <button type="button" className="btn btn-warning my-2 my-sm-0"><i className="fas fa-search"/></button>
-                    <button type="button" className="btn btn-warning categories dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <div class="collapse navbar-collapse text-right" id="navbarCollapse">
+            <ul class="navbar-nav m-auto">
+              
+              <li class="nav-item p-auto">
+                <div class="input-group  mx-auto">
+                  <div class="input-group-prepend ">
+                    <button type="button" class="btn btn-warning my-10 "><i class="fas fa-search"/></button>
+                    <button type="button" class="btn btn-warning categories dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                       Catégories
                     </button>
-                    <div className="dropdown-menu">
-                      <a className="dropdown-item" href="#">Fruits</a>
-                      <a className="dropdown-item" href="#">Légumes</a>
-                      <a className="dropdown-item" href="#">Lait et dérivées</a>
-                      <div role="separator" className="dropdown-divider"></div>
-                      <a className="dropdown-item" href="#">Separated link</a>
+                    <div class="dropdown-menu">
+                      <a class="dropdown-item">Fruits</a>
+                      <a class="dropdown-item">Légumes</a>
+                      <a class="dropdown-item">Lait et dérivées</a>
+                      <div role="separator" class="dropdown-divider"></div>
+                      <a class="dropdown-item">Separated link</a>
                     </div>
                   </div>
-                  <input className="form-control" type="text" placeholder="Search" aria-label="Search" />
+                  <input class="form-control w-50" type="text" placeholder="rechercher" aria-label="Rechercher" />
                 </div>
-              </li>
-              <li className="nav-item nav-link">
-                  <NavLink to="/Cart"style={{ color: "white" }} activeStyle={{ color: "white" }} >
-                    <i className="fas fa-shopping-cart"/> Panier
+              </li> 
+
+            </ul>
+            <ul class="navbar-nav float-right">
+
+              <li class="nav-item ">
+                <a class="nav-link"> <NavLink exact to="/" style={{ color: "white" }} activeStyle={{ color: "black" }} >
+                    <i class="fas fa-home" /> Accueil
                   </NavLink>
+                </a>
               </li>
-
-
+              <li class="nav-item">
+                <a class="nav-link">
+                  <NavLink to="/about" style={{ color: "white" }} activeStyle={{ color: "black" }} >
+                    <i class="fas fa-users" /> A propos
+                  </NavLink>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link ">
+                  <NavLink to="/SignIn" style={{ color: "white" }} activeStyle={{ color: "black" }} >
+                    <i class="fas fa-sign-in-alt" /> Connexion
+                  </NavLink>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link ">
+                  <NavLink to="/Cart"style={{ color: "white" }} activeStyle={{ color: "white" }} >
+                    <i class="fas fa-shopping-cart"/> Panier
+                  </NavLink>
+                </a>
+              </li>
+             
+              
             </ul>
           </div>
         </nav>
